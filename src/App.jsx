99d59aa9cac6296b41f117babaaaaa6247430aae
hellenpaula <helleribeiro29@gmail.com>
2026-04-09 13:53:55 -0300

    primeiro commit: aplicando projeto pronto no github

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState/* , useRef */ } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import SectionUI_UX from './components/Section_Servico_UI_UX/Section_Servico_UI_UX.jsx'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const carrossel = useRef(null);
+
+  return (
+   <div>
+    <SectionUI_UX />
+   </div>
+  )
+}
+
+export default App
